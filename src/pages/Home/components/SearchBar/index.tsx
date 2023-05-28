78e939d76e73f5efa97b67c58ed9b5622ef8dcbb
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-28 19:49:37 -0300

    feat: criando componente de barra de busca

---
+++ src/pages/Home/components/SearchBar/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from "./styles";
+
+const SearchBar: React.FC = () => {
+  return <Container>Pesquisar contato...</Container>
+};
+
+export { SearchBar };
---
+++ src/pages/Home/components/SearchBar/styles.ts
@@ -0,0 +1,7 @@
+import { CSS, styled } from "../../../../../stitches.config";
+
+const Container = styled('div', {
+
+} as CSS)
+
+export { Container };
