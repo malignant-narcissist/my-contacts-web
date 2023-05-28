d13d8d97258494579e889477eb136c5a5d73e068
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-28 19:34:25 -0300

    refactor: corrigindo o estilo e alinhamento do componente

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,6 +3,9 @@ import { CSS, styled } from '../../../stitches.config';
 const Container = styled('div', {
   display: 'flex',
   flexDirection: 'column',
+  alignItems: 'center',
+  padding: '65px 0',
+  gap: 35
 } as CSS);
 
 const Header = styled('div');
