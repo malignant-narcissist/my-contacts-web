f1106c08fc7e069aa101b8a5c82069607d00c454
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-28 18:35:14 -0300

    feat: criando esqueleto da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,32 @@
-import './styles.css';
+import { ContactList } from './components/ContactList';
+import { Container, Header, SearchBar } from './styles';
+
+// const ContactList = {
+//   CountingText: () => <>3 contatos</>,
+//   Button: () => <>Novo contato</>,
+//   OrdenationButton: () => <>Nome</>,
+//   Card: {
+//     Container: () => <></>,
+//     TextContent: {
+//       Container: () => <></>,
+//       Row: () => <></>,
+//     },
+//     ActionsContainer: () => <></>,
+//     Actions: {
+//       Container: () => <></>,
+//       Buttons: (({ children }) => <>{children}</>) as React.FC,
+//     },
+//   },
+// };
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Header>MyContacts</Header>
+      <SearchBar>Pesquisar</SearchBar>
+      <ContactList />
+    </Container>
+  );
 };
 
 export { Home };
