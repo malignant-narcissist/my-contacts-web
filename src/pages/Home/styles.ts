5cd7deeeb4536eca4680c7f28411b3e57da4a472
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-28 18:34:52 -0300

    feat: criando componentes primários de estilo da rota

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,12 @@
+import { CSS, styled } from '../../../stitches.config';
+
+const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+} as CSS);
+
+const Header = styled('div');
+
+const SearchBar = styled('div');
+
+export { Container, Header, SearchBar };
