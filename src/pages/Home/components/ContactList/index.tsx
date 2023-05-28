9b890324305f0a6c206c1f428c96ef6f3fdb2e1f
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-28 18:34:20 -0300

    feat: criando componente de lista de contatos

---
+++ src/pages/Home/components/ContactList/index.tsx
@@ -0,0 +1,7 @@
+import React from "preact/compat";
+
+const ContactList: React.FC = () => {
+  return <>3 contatos</>;
+};
+
+export { ContactList };
---
+++ src/pages/Home/components/ContactList/styles.ts
@@ -0,0 +1,5 @@
+import { CSS, styled } from '../../../../../stitches.config';
+
+const Container = styled('div', {} as CSS);
+
+export { Container };
