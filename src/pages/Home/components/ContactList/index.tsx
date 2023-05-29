85e638e1f920136bde8fc8ce6bb739a73ac78839
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-29 14:31:36 -0300

    feat: criando visual da lista de contatos

--- src/pages/Home/components/ContactList/index.tsx
+++ src/pages/Home/components/ContactList/index.tsx
@@ -1,7 +1,22 @@
-import React from "preact/compat";
+import React from 'preact/compat';
+import {
+  Container,
+  HeaderArea,
+  HeaderAreaContactCountText,
+  HeaderAreaCreateContactButton,
+} from './styles';
 
 const ContactList: React.FC = () => {
-  return <>3 contatos</>;
+  return (
+    <Container className="ContactList_Container">
+      <HeaderArea>
+        <HeaderAreaContactCountText>3 contatos</HeaderAreaContactCountText>
+        <HeaderAreaCreateContactButton>
+          Novo Contato
+        </HeaderAreaCreateContactButton>
+      </HeaderArea>
+    </Container>
+  );
 };
 
 export { ContactList };
--- src/pages/Home/components/ContactList/styles.ts
+++ src/pages/Home/components/ContactList/styles.ts
@@ -1,5 +1,40 @@
-import { CSS, styled } from '../../../../../stitches.config';
+import { CSS, styled, theme } from '../../../../../stitches.config';
 
-const Container = styled('div', {} as CSS);
+const Container = styled('div', {
+  borderBottom: `2px solid ${theme.colors.gray400.value}20`,
+  borderRadius: 1,
+  paddingBottom: 16,
+} as CSS);
 
-export { Container };
+const HeaderArea = styled('div', {
+  display: 'flex',
+  flex: 1,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+} as CSS);
+
+const HeaderAreaContactCountText = styled('span', {
+  color: '$dark',
+  fontSize: '$longer',
+  fontWeight: '$bold',
+} as CSS);
+
+const HeaderAreaCreateContactButton = styled('button', {
+  borderColor: '$primary200',
+  color: '$primary200',
+  borderWidth: 2,
+  borderStyle: 'solid',
+  borderRadius: 4,
+  background: 'none',
+  fontSize: '$regular',
+  fontWeight: '$bold',
+  padding: '10px 14px',
+} as CSS);
+
+export {
+  Container,
+  HeaderArea,
+  HeaderAreaContactCountText,
+  HeaderAreaCreateContactButton,
+};
