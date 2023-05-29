6215c2528c6be1158ffb747d27cd075556d55064
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-29 20:21:14 -0300

    refactor: inserindo componente de área da listagem com apenas o botão

--- src/pages/Home/components/ContactList/index.tsx
+++ src/pages/Home/components/ContactList/index.tsx
@@ -1,20 +1,29 @@
 import React from 'preact/compat';
+import { ArrowUpIcon } from '../../assets';
 import {
   Container,
   HeaderArea,
   HeaderAreaContactCountText,
   HeaderAreaCreateContactButton,
+  ListArea,
+  ListOrderButton,
+  ListOrderIcon,
 } from './styles';
 
 const ContactList: React.FC = () => {
   return (
-    <Container className="ContactList_Container">
+    <Container>
       <HeaderArea>
         <HeaderAreaContactCountText>3 contatos</HeaderAreaContactCountText>
         <HeaderAreaCreateContactButton>
           Novo Contato
         </HeaderAreaCreateContactButton>
       </HeaderArea>
+      <ListArea>
+        <ListOrderButton>
+          Nome <ListOrderIcon src={ArrowUpIcon} />
+        </ListOrderButton>
+      </ListArea>
     </Container>
   );
 };
