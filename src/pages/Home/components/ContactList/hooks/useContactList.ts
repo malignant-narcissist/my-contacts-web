f5bb0db71920f5c44ae61c4c1379db5d81173069
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-29 21:01:25 -0300

    feat: criando estado e dispatch para controlar a ascendência da ordenação

--- src/pages/Home/components/ContactList/hooks/useContactList.ts
+++ src/pages/Home/components/ContactList/hooks/useContactList.ts
@@ -1,3 +1,4 @@
+import { Reducer, useReducer } from 'preact/hooks';
 import { ImmerReducer, useImmerReducer } from 'use-immer';
 
 type State = Record<string, unknown>[];
@@ -11,12 +12,19 @@ const reducer: ImmerReducer<State, Actions> = (draft, actions) => {
   return draft;
 };
 
+const orderAscReducer: Reducer<'ASC' | 'DESC', undefined> = (state) => {
+  return state === 'ASC' ? 'DESC' : 'ASC'
+}
+
 const useContactList = () => {
   const [cardList, dispatchCardList] = useImmerReducer(reducer, []);
+  const [orderAsc, toggleOrderAsc] = useReducer(orderAscReducer, 'ASC');
 
   return {
     cardList,
     dispatchCardList,
+    orderAsc,
+    toggleOrderAsc,
   };
 };
 
