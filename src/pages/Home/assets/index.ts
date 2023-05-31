1ba54e7f31806f0cd8d37ee77a086f5b865b627f
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-31 17:03:45 -0300

    feat: adicionando imagens dos botões de ação dos cards

--- src/pages/Home/assets/index.ts
+++ src/pages/Home/assets/index.ts
@@ -1,4 +1,6 @@
 import ArrowUpIcon from './arrow-up.svg';
+import EditIcon from './edit.svg';
 import HeaderImage from './header.svg';
+import TrashIcon from './trash.svg';
 
-export { HeaderImage, ArrowUpIcon };
+export { HeaderImage, ArrowUpIcon, EditIcon, TrashIcon };
