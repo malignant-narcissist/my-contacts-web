c31a5aab89d8aba0054f66d467e47c868b8f08dd
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-05-31 17:21:13 -0300

    feat: declarando nomeadamente componentes estilizados do card

---
+++ src/pages/Home/components/ContactList/Card/styles.ts
@@ -0,0 +1,34 @@
+import { CSS, styled } from '../../../../../../stitches.config';
+
+const Container = styled('div', {} as CSS);
+
+const ContentArea = styled('div', {} as CSS);
+
+const ContentRow = styled('div', {} as CSS);
+
+const ContactName = styled('strong', {} as CSS);
+
+const ContactSocialMedia = styled('span', {} as CSS);
+
+const ContactMail = styled('span', {} as CSS);
+
+const ContactPhone = styled('span', {} as CSS);
+
+const ButtonEdit = styled('button', {} as CSS);
+
+const ButtonRemove = styled('button', {} as CSS);
+
+const ButtonImage = styled('img', {} as CSS);
+
+export {
+  Container,
+  ButtonImage,
+  ButtonRemove,
+  ButtonEdit,
+  ContactPhone,
+  ContactMail,
+  ContactName,
+  ContactSocialMedia,
+  ContentArea,
+  ContentRow,
+};
