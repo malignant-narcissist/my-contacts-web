c0327746434345d83d694a46426c0ab00665bb0d
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-02 20:17:19 -0300

    refactor: habilitando manipulação dos map e set do js pelo immer

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
 import { applyGlobalStyles } from './components/GlobalStyles/styles';
 import Routes from './routes';
 
+import { enableMapSet } from 'immer';
 
+enableMapSet()
 
 export const App = () => {
   applyGlobalStyles();
