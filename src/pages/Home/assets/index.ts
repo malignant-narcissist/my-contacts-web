fecff41d3654fb23519d6f6adb2c6794d5077577
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-03 16:44:23 -0300

    feat: criando imagem de emoji triste

--- src/pages/Home/assets/index.ts
+++ src/pages/Home/assets/index.ts
@@ -1,6 +1,7 @@
 import ArrowUpIcon from './arrow-up.svg';
 import EditIcon from './edit.svg';
 import HeaderImage from './header.svg';
+import SadFaceEmojiImage from './sad-face-emoji.svg';
 import TrashIcon from './trash.svg';
 
-export { HeaderImage, ArrowUpIcon, EditIcon, TrashIcon };
+export { HeaderImage, ArrowUpIcon, EditIcon, TrashIcon, SadFaceEmojiImage };
