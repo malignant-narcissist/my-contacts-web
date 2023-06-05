9af1e54122da79c57a3ecde0b889efacb3d40d56
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-05 18:21:34 -0300

    refactor: ativando servidor quando em ambiente de desenvolvimento

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
-import { render } from 'preact';
 import { App } from './app.tsx';
+import { makeServer } from './server';
+import { render } from 'preact';
+
+if (import.meta.env.DEV) {
+  makeServer();
+}
 
 render(<App />, document.getElementById('app') as HTMLElement);
