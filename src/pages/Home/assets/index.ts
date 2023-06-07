e0e8d5edefa2d167182b5739f92b2f98561291ce
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-06 22:23:07 -0300

    feat: adicionando e exportando imagem

--- src/pages/Home/assets/index.ts
+++ src/pages/Home/assets/index.ts
@@ -1,7 +1,15 @@
 import ArrowUpIcon from './arrow-up.svg';
 import EditIcon from './edit.svg';
+import EmptyBoxImage from './empty-box.svg';
 import HeaderImage from './header.svg';
 import SadFaceEmojiImage from './sad-face-emoji.svg';
 import TrashIcon from './trash.svg';
 
-export { HeaderImage, ArrowUpIcon, EditIcon, TrashIcon, SadFaceEmojiImage };
+export {
+  HeaderImage,
+  ArrowUpIcon,
+  EditIcon,
+  EmptyBoxImage,
+  TrashIcon,
+  SadFaceEmojiImage,
+};
