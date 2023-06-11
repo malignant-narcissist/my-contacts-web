d3782fba525ddf5689ed1d6b2406d8b2726e28ba
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-11 16:44:00 -0300

    refactor: adicionando props ao componente de barra de busca

--- src/pages/Home/components/SearchBar/index.tsx
+++ src/pages/Home/components/SearchBar/index.tsx
@@ -1,9 +1,16 @@
 import { Container, ControlledInput } from './styles';
+import React, { HTMLAttributes } from 'preact/compat';
 
-const SearchBar: React.FC = () => {
+type Props = Pick<HTMLAttributes<HTMLInputElement>, 'value' | 'onSearch'>;
+const SearchBar: React.FC<Props> = (props) => {
   return (
     <Container>
-      <ControlledInput type='text' placeholder='Pesquisar contato...' />
+      <ControlledInput
+        type='search'
+        name='filterName'
+        placeholder='Pesquisar contato...'
+        {...props}
+      />
     </Container>
   );
 };
