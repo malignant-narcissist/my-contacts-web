42701993cffa0ec48a75866dea6dcb9d0c2c4772
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-11 20:49:03 -0300

    feat: habilitando função de busca por contatos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,14 +2,28 @@ import { HeaderImage } from './assets';
 import { ContactList } from './components/ContactList';
 import { SearchBar } from './components/SearchBar';
 import { Container, ContentContainer, Header } from './styles';
+import { useState } from 'preact/hooks';
 
 const Home = () => {
+  const [filterName, setFilterName] = useState<string>();
+
   return (
     <Container>
       <Header src={HeaderImage} />
       <ContentContainer>
-        <SearchBar />
-        <ContactList />
+        <SearchBar
+          value={filterName}
+          onSearch={(e) => {
+            if (
+              e.target &&
+              'value' in e.target &&
+              typeof e.target.value === 'string'
+            ) {
+              setFilterName(e.target.value);
+            }
+          }}
+        />
+        <ContactList filterName={filterName} />
       </ContentContainer>
     </Container>
   );
