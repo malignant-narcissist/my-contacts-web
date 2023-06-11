b418ab4724a962b0ee9f425601ede9668e65dd90
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-11 20:36:06 -0300

    refactor: modelando e seedando banco do miragejs

--- src/server.ts
+++ src/server.ts
@@ -2,26 +2,47 @@ import { CardType } from './pages/Home/components/ContactList/hooks/types';
 import { Response, createServer } from 'miragejs';
 
 const makeServer = () => {
-  // rome-ignore lint/style/useConst: <explanation>
-  let server = createServer({
+  const server = createServer({
+    seeds(server) {
+      server.schema.db.createCollection('contacts', [
+        {
+          id: '1',
+          email: 'asta@mail.com',
+          name: 'Asta Zogratis',
+          phone: '(66) 66666-6666',
+        },
+        {
+          id: '2',
+          email: 'lucius@mail.com',
+          name: 'Lucius Zogratis',
+          phone: '(91) 97800-8768'
+        },
+        {
+          id: '3',
+          email: 'yuno@mail.com',
+          name: 'Yuno Grinberryall',
+          phone: '(69) 95472-4567'
+        }
+      ] satisfies CardType[]);
+    },
     routes() {
       this.urlPrefix = import.meta.env.VITE_API_BASE_URL;
 
       this.namespace = 'contacts';
 
-      this.get(
-        '',
-        () => {
-          return new Response(200, undefined, [
-            {
-              id: '1',
-              email: 'asta@mail.com',
-              name: 'Asta Zogratis',
-              phone: '(66) 66666-6666',
-            },
-          ] satisfies CardType[]);
-        },
-      );
+      this.get('', (schema, request) => {
+        const { queryParams } = request;
+
+        return new Response(
+          200,
+          undefined,
+          queryParams.name
+            ? schema.db.contacts.filter((item: CardType) => {
+                return item.name.includes(queryParams.name);
+              })
+            : schema.db.contacts,
+        );
+      });
     },
   });
 
