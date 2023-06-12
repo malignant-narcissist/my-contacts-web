39ef4ffb4db16fd05c4ec6748a17288cc078b890
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-12 18:26:14 -0300

    refactor: indexando página na rota

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Home } from '../pages/Home';
 import { Route, Router } from 'wouter-preact';
+import { AddContact } from "../pages/AddContact";
 
 const routes = () => {
   return (
     <Router>
       <Route path='/' component={Home} />
+      <Route path="/add" component={AddContact} />
     </Router>
   );
 };
