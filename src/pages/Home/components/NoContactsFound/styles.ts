9a7de7ef1550e5ce8c50aa4b8e549221d8b1146b
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-13 15:37:54 -0300

    refactor: aplicando estilos da página

--- src/pages/Home/components/NoContactsFound/styles.ts
+++ src/pages/Home/components/NoContactsFound/styles.ts
@@ -1,12 +1,29 @@
-import { getStyled } from '../../../../../stitches.config';
+import { CSS, getStyled } from '../../../../../stitches.config';
 import { HTMLAttributes } from 'preact/compat';
 
 const Image = getStyled<HTMLAttributes<HTMLImageElement>>('img');
 
-const Container = getStyled('div', {});
+const Container = getStyled('div', {
+  display: 'flex',
+  width: '100%',
+  gap: 24,
+  alignItems: 'center',
+});
 
-const Text = getStyled('p', {});
+const textStyles: CSS = {
+  color: '$gray400',
+  fontSize: '$regular',
+}
 
-const BoldText = getStyled('span', {});
+const Text = getStyled('p', {
+  flex: 1,
+  fontWeight: '$regular',
+  ...textStyles,
+});
+
+const BoldText = getStyled('span', {
+  ...textStyles,
+  fontWeight: '$bold',
+});
 
 export { BoldText, Container, Image, Text };
