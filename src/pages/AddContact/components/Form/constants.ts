a98ad99f36a5d94efb769e0594721eeba322fbc8
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-06-25 15:04:05 -0300

    refactor: removendo constante não utilizada

--- src/pages/AddContact/components/Form/constants.ts
+++ src/pages/AddContact/components/Form/constants.ts
@@ -1,4 +1,4 @@
-import { enums, object, pattern, string } from 'superstruct';
+import { enums, pattern, string } from 'superstruct';
 
 export const FORM_FIELDS = {
   NAME: 'name',
@@ -12,22 +12,6 @@ export const FIELDS_PLACEHOLDERS = {
   [FORM_FIELDS.EMAIL]: 'E-mail',
   [FORM_FIELDS.PHONE]: 'Telefone',
 } as const;
-
-export const ContactStructure = object({
-  [FORM_FIELDS.NAME]: string(),
-  [FORM_FIELDS.EMAIL]: pattern(
-    string(),
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-  ),
-  [FORM_FIELDS.PHONE]: pattern(string(), /\([0-9]{2}\) 9[0-9]{4}-[0-9]{4}/),
-  [FORM_FIELDS.CATEGORY]: enums([
-    'instagram',
-    'facebook',
-    'whatsapp',
-    'telegram',
-    'none',
-  ]),
-});
 
 export const ContactFieldsValidations = {
   [FORM_FIELDS.NAME]: string(),
