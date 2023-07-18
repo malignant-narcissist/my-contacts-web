a9ac3578d27da14fad0177049b150ed3ff254377
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2023-07-18 00:17:57 -0300

    feat: criando função para mascarar o telefone

---
+++ src/shared/utils/formatPhone.ts
@@ -0,0 +1,7 @@
+const formatPhone = (phoneNumber: string) =>
+  phoneNumber
+    .replace(/\D/g, '')
+    .replace(/^(\d{2})\B/, '($1) ')
+    .replace(/(\d)?(\d{4})(\d{4})/, '$1$2-$3');
+
+export { formatPhone }
